1f1747e93c5fd91dd86382c1591ae98895d3f92a
Eventuallz <nagy.hajnalka.eva@bene-edu.ch>
2024-12-06 13:38:52 +0100

    Login site created

---
+++ AbsenzAPPDBHosting/Controllers/Login.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyApp.Namespace
+{
+    public class Login : Controller
+    {
+        // GET: Login
+        public IActionResult In()
+        {
+            return View();
+        }
+    }
+}
