461c80bd6e389269eaef4909c39f16062317c31a
Eventuallz <nagy.hajnalka.eva@bene-edu.ch>
2024-12-17 17:06:19 +0100

    Add basic login page; add .idea to gitignore

---
+++ AbsenzAPPDBHosting/Models/LoginViewModel.cs
@@ -0,0 +1,9 @@
+﻿
+namespace AbsenzAPPDBHosting.Models;
+
+public class LoginViewModel
+{
+    public required string Username { get; set; }
+    
+    public required string Password { get; set; }
+}
