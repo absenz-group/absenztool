73a673840eda4cf8fabbb5b044a36a8de53e329e
studentNr-1 <102148944+studentNr-1@users.noreply.github.com>
2025-01-10 09:20:41 +0100

    Schüleransicht - TWI

---
+++ AbsenzAPPDBHosting/Controllers/SchulerController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AbsenzAPPDBHosting.Controllers
+{
+    public class SchulerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("GesamtUbersicht");
+        }
+    }
+}
---
+++ AbsenzAPPDBHosting/Models/SchulerAbsenzUbersicht/SchulerUbersicht.cs
@@ -0,0 +1,14 @@
+﻿namespace AbsenzAPPDBHosting.Models.SchulerAbsenzUbersicht
+{
+    public class SchulerUbersicht
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Klasse { get; set; }
+        public List<Eltern> Eltern { get; set; }
+
+        public List<Absenzen> Absenzen { get; set; }
+        public int UnentschuldigtCount { get; set; }
+        public int EntschuldigtCount { get; set; }
+    }
+}
