553a2fba797036ca13d5630e6ab411fc45c3b263
studentNr-1 <102148944+studentNr-1@users.noreply.github.com>
2025-01-22 22:40:25 +0100

    Demo - Tobi

---
+++ AbsenzAPPDBHosting/BusinessLogic2/LehrerLogic.cs
@@ -0,0 +1,47 @@
+﻿using AbsenzAPPDBHosting.Data;
+using AbsenzAPPDBHosting.Models;
+using AbsenzAPPDBHosting.Models.SchulerAbsenzUbersicht;
+
+namespace AbsenzAPPDBHosting.BusinessLogic2
+{
+    public class LehrerLogic
+    {
+
+        private readonly ApplicationDbContext _context;
+
+        public LehrerLogic(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<Schueler> GetSchulerListe()
+        {
+            List<Schueler> schulerliste = new List<Schueler>();
+            try
+            {
+                schulerliste = _context.Schueler.ToList();
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return schulerliste;
+        }
+
+        public List<Absenzgruende> GetAbsenzgruende()
+        {
+            List<Absenzgruende> absenzGruendeList = new List<Absenzgruende>();
+
+            try
+            {
+                absenzGruendeList = _context.Absenzgruende.ToList();
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            return absenzGruendeList;
+        }
+    }
+}
---
+++ AbsenzAPPDBHosting/Controllers/LehrerController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AbsenzAPPDBHosting.Controllers
+{
+    public class LehrerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("~/Views/LehrerAnsichten/LehrerFilterPage.cshtml");
+        }
+    }
+}
